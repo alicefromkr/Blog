cc81979d350817a9697b1f7e8f7ce4e486001bb0
Alice <jaeyeon0921@gmail.com>
2024-03-16 19:46:33 +0900

    Initial setup with initial comment

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Alice's Study Note</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/alicefromkr",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/alicefromkr/Blog",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Alice's Blog powered by Nextra Docs",
   },
-}
+};
 
-export default config
+export default config;
